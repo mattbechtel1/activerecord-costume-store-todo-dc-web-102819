b6df278ec28f57d99085e5248a91719c75e0146c
Matt Bechtel <bechma02@gmail.com>
2019-11-10 03:38:05 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,11 +5,12 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
     create_table :haunted_houses do |t|
       t.string :name
       t.string :location
-      t.integer :costume_inventory
-      t.integer :employee_count
-      t.boolean :open?
-      t.datetime :opening_time
-      t.datetime :closing_time
+      t.string :theme
+      t.float :price
+      t.boolean :family_friendly?
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.text :long_description
     end
   end
 
