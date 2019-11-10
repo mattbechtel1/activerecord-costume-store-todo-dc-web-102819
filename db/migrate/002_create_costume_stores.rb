b7deefda75b28f1ef0f31df1f3a298a0f8878e64
Matt Bechtel <bechma02@gmail.com>
2019-11-10 03:35:23 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.string :image_url
+      t.timestamps
+    end
+  end
+
+end
